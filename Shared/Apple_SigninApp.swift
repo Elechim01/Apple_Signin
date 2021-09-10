ce3446ee6cd99b870c16f9fcd99a3a4e9f6ee5bd
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-10 15:34:32 +0200

    Initial Commit

---
+++ Shared/Apple_SigninApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_SigninApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_SigninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
